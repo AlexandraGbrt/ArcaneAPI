25dbd95d6b697ddce6ed65f24378de2f23669dab
Alexandra Gbrt <alexandra.gaubert95@gmail.com>
2025-06-17 18:48:02 +0200

    ajout tables SQLAlchemy

--- models.py
+++ models.py
@@ -13,4 +13,17 @@ class Personnage(Base):
 class Relation(Base):
     __tablename__ = "relations"
 
+    perso_1_id = Column(Integer, primary_key=True,)
+    perso_2_id = Column(Integer, primary_key=True)
+    type_relation = Column(String, nullable=False)
+
+
+
+class Lieu(Base):
+    __tablename__ = "lieux"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nom = Column(String(100), nullable=False)
+    description = Column(String(255), nullable=False)
+    type = Column(String(50), nullable=False)
     
